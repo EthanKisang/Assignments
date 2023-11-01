7571947fd4943d80ba56ebed69828bc681201e93
EthanKisang <146781528+EthanKisang@users.noreply.github.com>
2023-11-01 09:31:24 +0300

    Create SumOf3Numbers.c
    This is my first assignment uploaded to github

---
+++ SumOf3Numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a, b,c,d;
+    printf("Enter the first number \n");
+    scanf("%d",&a);
+    printf("Enter the second number \n");
+    scanf("%d",&b);
+    printf("Enter the third number \n");
+    scanf("%d",&c);
+    d=a+b+c;
+    printf("%d + %d + %d = %d\n",a,b,c,d);
+}
